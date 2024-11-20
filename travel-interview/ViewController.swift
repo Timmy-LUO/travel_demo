7f2448ef9d973dd423eb3f8afcc580590064fb5d
Timmy Luo <a220009996@gmail.com>
2024-11-20 12:54:14 +0800

    Initial Commit

---
+++ travel-interview/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  travel-interview
+//
+//  Created by ZHI on 2024/11/20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
